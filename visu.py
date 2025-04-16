5bc6027dfb8c622d37496060448d5138d0132cdd
Carsten Knoll <carsten.knoll@posteo.de>
2025-04-16 15:44:28 +0200

    add visualization of whole graph (needs pyirk > 0.14.2)
    # Last 3 commits:
    
    # b22a167 trivial changes in visu.py 
    # b1bdeb9 add at al to sparql result 
    # 8a27c6e remove pcl files from repo

--- visu.py
+++ visu.py
@@ -1,5 +1,6 @@
 import os
 
+from packaging import version
 import pyirk as p
 
 from ipydex import IPS
@@ -18,7 +19,43 @@ def visualize(label, radius):
         f.write(vis)
 
 ###################
-visualize("Pt", 1)
-visualize("Pt", 2)
+visualize("platinum", 1)
+visualize("platinum", 2)
+
+
+
+def prepare_graph():
+    """
+    Delete some entities from OCSE (e.g. control-theory related persons).
+
+    Motivation: We use the ocse-agent-module e.g. to model authors of Memristor-papers;
+    However we do not need to include the humans modeled in that module.
+    """
+
+    print("select human-instances from ocse/agents1.py")
+    humans_to_delete = []
+
+    assert version.parse(p.__version__) >= version.parse("0.14.2")
+
+
+    for human in mod1.ag.I7435.get_inv_relations("R4", return_subj=True):
+        if mod1.ag.__URI__ in human.uri:
+            humans_to_delete.append(human)
+
+    for human in humans_to_delete:
+        p.ds.statements.pop(human.uri)
+
+    return None
+
+
+
+def visualize_whole_graph():
+    prepare_graph()
+
+    vis = p.visualize_all_entities()
+    with open(f"vis/whole_graph.svg", "wt", encoding="utf-8") as f:
+        f.write(vis)
+
+visualize_whole_graph()
 
 IPS()

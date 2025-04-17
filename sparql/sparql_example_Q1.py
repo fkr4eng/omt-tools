664174ab5d7e22d4f242932289b1a1624474a5de
RST98\Julius Fiedler <ichderjulius@gmx.de>
2025-04-17 14:39:37 +0200

    new sparql query + renaming

---
+++ sparql/sparql_example_Q1.py
@@ -0,0 +1,68 @@
+import os
+
+import pyirk as p
+from ipydex import IPS
+
+from stafo.utils import config_data
+
+path = "output.py"
+
+mod1 = p.irkloader.load_mod_from_path(path, "mem")
+ag_mod = mod1.ag
+omt_mod = mod1.omt
+
+# dynamically get the short keys since they might change if the knowledge graph is updated
+paper_2019_xia = p.ds.get_item_by_label("publication: Memristive crossbar arrays for brain-inspired computing").short_key
+paper_2024_aguirre = p.ds.get_item_by_label("publication: Hardware implementation of memristorbased artificial neural networks").short_key
+paper_2019_lanza = p.ds.get_item_by_label("publication: Recommended Methods to Study Resistive Switching Devices").short_key
+I_mem_stack = p.ds.get_item_by_label("memristor stack")
+I_stack_comp = p.ds.get_item_by_label("stack component")
+for k, v in p.ds.relations.items():
+    if mod1.__URI__ in k:
+        if "has stack component" in v.R1:
+            R_has_st_com = k.split("#")[-1]
+        elif "has memristor stack" in v.R1:
+            R_has_mem_st = k.split("#")[-1]
+        elif "has position" in v.R1:
+            R_has_pos = k.split("#")[-1]
+        elif "is at outer position" in v.R1:
+            R_is_outer = k.split("#")[-1]
+        elif "has citation id" in v.R1:
+            R_cit_id = k.split("#")[-1]
+        elif "has internal reference" in v.R1:
+            R_int_ref = k.split("#")[-1]
+
+p.ds.rdfgraph = p.rdfstack.create_rdf_triples(add_qualifiers=True)
+
+################################################################################
+# query
+################################################################################
+
+print("Search for publications that appear in multiple overview papers.")
+qsrc = f"""
+PREFIX : <{p.rdfstack.IRK_URI}>
+PREFIX qf: <{mod1.__URI__}/QUALIFIERS#>
+PREFIX omt: <{omt_mod.__URI__}#>
+PREFIX ag: <{ag_mod.__URI__}#>
+PREFIX ag_s: <{ag_mod.__URI__}/STATEMENTS#>
+PREFIX ag_p: <{ag_mod.__URI__}/PREDICATES#>
+PREFIX mem: <{mod1.__URI__}#>
+PREFIX mem_s: <{mod1.__URI__}/STATEMENTS#>
+PREFIX mem_p: <{mod1.__URI__}/PREDICATES#>
+SELECT ?overview_paper ?citation_number ?component ?stack
+WHERE {{
+    ?paper1 ag:R8440__cites ?pub.
+    ?paper2 ag:R8440__cites ?pub.
+
+    FILTER (?paper1 != ?paper2)
+
+    ?paper1 mem:{R_int_ref}__has_internal_reference ?overview_paper.
+    ?paper2 mem:{R_int_ref}__has_internal_reference ?overview_paper.
+}}
+"""
+p.ds.rdfgraph = p.rdfstack.create_rdf_triples(add_qualifiers=True)
+res1 = p.rdfstack.perform_sparql_query(qsrc)
+table = p.rdfstack.query_result_to_table(res1, labels_only=True)
+print(table)
+table.to_csv("sparql/res/queryQ1.csv")
+IPS()
